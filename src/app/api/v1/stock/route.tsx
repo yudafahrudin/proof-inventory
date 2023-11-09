2862fec81dc3ba2bd36ab8f51bcf395e7fec1e47
yudafahrudin <ahmadyudafahrudin@gmail.com>
2023-11-10 00:47:01 +0700

    refactor: update path

--- src/app/api/v1/stock/route.tsx
+++ src/app/api/v1/stock/route.tsx
@@ -1,9 +1,11 @@
 import { NextRequest, NextResponse } from "next/server";
 import { writeFile, readFile } from "fs/promises";
+import path from "path";
 
 export async function GET() {
   try {
-    const data = await readFile("./src/assets/proof-stock.json");
+    const file = path.join(process.cwd(), "src/assets/", "proof-stock.json");
+    const data = await readFile(file, "utf8");
     return NextResponse.json({ data: data.toString() });
   } catch (error) {
     return NextResponse.json({ error });
